d7e26ca99c02f45788eade4c229cfdd7d7b685c1
maze <you@kred730036@gmail.com>
2024-04-18 17:38:31 +0900

    사람1

--- src/com/tenco/temp2/Person1.java
+++ src/com/tenco/temp2/Person1.java
@@ -1,5 +1,7 @@
 package com.tenco.temp2;
 
+
+
 public class Person1 {
 	
 	//체력, 속도, 방어력, 공격력
@@ -19,19 +21,86 @@ public class Person1 {
 		defense = 1;
 		power = 1;
 	}
-	
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getHp() {
+		return hp;
+	}
+
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+
+	public int getSpeed() {
+		return speed;
+	}
+
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+
+	public int getDefense() {
+		return defense;
+	}
+
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+
+	public int getPower() {
+		return power;
+	}
 
 
 	
+	public void setPower(int power) {
+		this.power = power;
+	}
 	
 	
-	//공격 당해서 죽음
-	/*
-	public void beAttacked(int power) {
+	
+		
+	
+	
+	
+
+
+	//////
+
+
+	//공격을 하다
+	public void attackPerson(Person1 person) {
+		System.out.println("공격(" + this.power + ") 합니다");
+		person.beAttackPerson(this.power); // 호출
+	}
+	
+	// 내가 공격을 받다.
+	public void beAttackPerson(int power) {
 		if ( hp <= 0 ) {
 			System.out.println("당신은 죽었습니다");
 			hp = 0;
 			return;
 		}
-	}*/
+		hp -= power;
+
+	
+	
+	}
+	
 }
