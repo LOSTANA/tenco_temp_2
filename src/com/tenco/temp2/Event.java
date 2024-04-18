1a56338e472705b7cce692f7885a93398ed775ef
cnow <wjdtpdbs1887@naver.com>
2024-04-18 16:26:19 +0900

    이벤트 틀 잡음

---
+++ src/com/tenco/temp2/Event.java
@@ -0,0 +1,62 @@
+package com.tenco.temp2;
+
+public class Event {
+	private int rain;
+	private int wind;
+	private int thunderbolt;
+	private int normal;
+
+	// getter
+	public int getRain() {
+		return rain;
+	}
+
+	public void setRain(int rain) {
+		this.rain = rain;
+	}
+
+	public int getWind() {
+		return wind;
+	}
+
+	public void setWind(int wind) {
+		this.wind = wind;
+	}
+
+	public int getThunderbolt() {
+		return thunderbolt;
+	}
+
+	// 기능
+	public void Normal() {
+		System.out.println("날씨가 맑습니다.");
+	}
+
+	public void setThunderbolt(int thunderbolt) {
+		this.thunderbolt = thunderbolt;
+	}
+
+	public int getNormal() {
+		return normal;
+	}
+
+	public void setNormal(int normal) {
+		this.normal = normal;
+	}
+
+	public void Rainstorm (int rain) {
+		System.out.println("비가 크게 내립니다.");
+		misfire;
+	}
+
+	public void Windstorm (int wind) {
+		System.out.println("바람이 심하게 붑니다.");
+		Accuracy;
+	}
+
+	public void Thunderbolt (int thunderbolt) {
+		System.out.println("번개가 몰아칩니다.");
+		stun;
+	}
+
+}
