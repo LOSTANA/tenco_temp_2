7c619a9fbb64de1c48ede32705ab4e09a9740105
cnow <wjdtpdbs1887@naver.com>
2024-04-18 17:20:18 +0900

    이벤트 처리3

--- src/com/tenco/temp2/Event.java
+++ src/com/tenco/temp2/Event.java
@@ -29,13 +29,18 @@ public class Event {
 	public void Normal() {
 		System.out.println("날씨가 맑습니다.");
 	}
+	
 	public void Rainstorm () {
-		System.out.println("비가 크게 내립니다.");
-
+		System.out.println(" 비가 크게 내려 ");
 	}
 
-	public void Windstorm () {
-		System.out.println("바람이 심하게 붑니다.");
+	public void Windstorm1 (Person1 p1) {
+		System.out.println("바람이 p1에게 유리하게 붑니다.");
+		
+	}
+	
+	public void Windstorm2 (Person2 p2) {
+		System.out.println("바람이 p2에게 유리하게 붑니다.");
 
 	}
 
