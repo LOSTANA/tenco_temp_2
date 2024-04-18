eb455feb0744d9b0ab9de8c97bbdf257183f60c9
maze <you@kred730036@gmail.com>
2024-04-18 17:49:49 +0900

    사람1

--- src/com/tenco/temp2/Person1.java
+++ src/com/tenco/temp2/Person1.java
@@ -1,106 +1,86 @@
 package com.tenco.temp2;
 
-
-
 public class Person1 {
-	
-	//체력, 속도, 방어력, 공격력
-	
-	String name;
-	private int hp; //체력
-	private int speed; //속도
-	private int defense; //방어력
-	private int power; //공격력
-	
-	
-	//?
-	public void person1(String myName) {
-		name = myName;
+
+	private String name;
+	private int hp; // 체력
+	private int speed; // 속도
+	private int sp; // 방어력
+	private int ap; // 공격력
+
+	// 스탯
+	public void person1() {
+		name = "p1";
 		hp = 100;
 		speed = 1;
-		defense = 1;
-		power = 1;
+		sp = 1;
+		ap = 1;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public int getHp() {
 		return hp;
 	}
 
-
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
-
 
 	public int getSpeed() {
 		return speed;
 	}
 
-
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
 
-
 	public int getDefense() {
-		return defense;
+		return sp;
 	}
-
 
 	public void setDefense(int defense) {
-		this.defense = defense;
+		this.sp = defense;
 	}
-
 
 	public int getPower() {
-		return power;
+		return ap;
 	}
 
-
-	
 	public void setPower(int power) {
-		this.power = power;
+		this.sp = power;
 	}
-	
-	
-	
-		
-	
-	
-	
-
 
 	//////
 
-
-	//공격을 하다
+	// 공격을 하다
 	public void attackPerson(Person1 person) {
-		System.out.println("공격(" + this.power + ") 합니다");
-		person.beAttackPerson(this.power); // 호출
+		System.out.println("공격(" + this.ap + ") 합니다");
+		person.beAttackPerson(this.ap); // 호출
 	}
-	
-	// 내가 공격을 받다.
-	public void beAttackPerson(int power) {
-		if ( hp <= 0 ) {
+
+	// 공격을 받다.
+	public void beAttackPerson(int ap) {
+		if (hp <= 0) {
 			System.out.println("당신은 죽었습니다");
 			hp = 0;
 			return;
 		}
-		hp -= power;
-
-	
-	
+		hp -= ap;
+		
 	}
 	
-}
+	//이벤트 발생
+	// rain;
+	// wind;
+	// thunderbolt;
+	// normal;
+
+
+}//end of class
