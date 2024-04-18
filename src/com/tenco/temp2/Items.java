14fb34f7a33bad523b64cd64b16aadd65338136b
ryipuxt <happy201732@gmail.com>
2024-04-18 17:51:26 +0900

    사람별 아이템(미완)

--- src/com/tenco/temp2/Items.java
+++ src/com/tenco/temp2/Items.java
@@ -2,45 +2,106 @@ package com.tenco.temp2;
 
 public class Items {
 
-
-	public void showInfo() {
-		System.out.println("======== 아이템 목록 ========");
-		System.out.println("포션");
-		System.out.println("리볼버1, 리볼버2");
-		System.out.println("권총1, 권총2");
-		System.out.println("총집");
-		System.out.println("방탄복");
-		System.out.println("=============================");
-	}
-
-	public void usePotion() {
-		hp += 15;
-	}
-
-	public void equipRevolver1() {
-		power += 5;
+// Person1
+	
+	public void usePotionP1(Person1 person1) {
+		int p1Hp = person1.getHp();
+		int setHp1 =(p1Hp += 15);
+		person1.setHp(setHp1);
 	}
 	
-	public void equipRevolver2() {
-		power += 10;
+	public void equipRevolver1P1(Person1 person1) {
+		int p1Ap = person1.getAp();
+		int setAp1 =(p1Ap += 3);
+		person1.setAp(setAp1);
 	}
 	
-	public void equipPistol1() {
-		power += 3;
-		speed += 5;
+	public void equipRevolver1P1(Person1 person1) {
+		int p1Ap = person1.getAp();
+		int setAp1 =(p1Ap += 7);
+		person1.setAp(setAp1);
 	}
 	
-	public void equipPistol2() {
-		power += 7;
-		speed += 10;
+	public void equipPistol1P1(Person1 person1) {
+		int p1Ap = person1.getAp();
+		int p1Speed = person1.getSpeed();
+		int setAp1 =(p1Ap += 1);
+		int setSpeed1 =(p1Speed += 5);
+		person1.setAp(setAp1);
+		person1.setSp(setSpeed1);
+		
 	}
-
-	public void equipHolster() {
-		speed += 10;
+	
+	public void equipPistol1P1(Person1 person1) {
+		int p1Ap = person1.getAp();
+		int p1Speed = person1.getSpeed();
+		int setAp1 =(p1Ap += 4);
+		int setSpeed1 =(p1Speed += 10);
+		person1.setAp(setAp1);
+		person1.setSp(setSpeed1);
 	}
-
-	public void equipArmor() {
-		defense += 10;
+	
+	public void equipHolsterP1(Person1 person1) {
+		int p1Speed = person1.getSpeed();
+		int setSpeed1 =(p1Speed += 15);
+		person1.setSp(setSpeed1);
+	}
+	
+	public void equipArmorP1(Person1 person1) {
+		int p1Sp = person1.getSp();
+		int setSp1 =(p1Sp += 1);
+		person1.setSp(setSp1);
+	}
+	
+// Person2
+	
+	public void usePotionP2(Person2 person2) {
+		int p2Hp = person2.getHp();
+		int setHp2 =(p2Hp += 15);
+		person2.setHp(setHp2);
+	}
+	
+	public void equipRevolver1P2(Person2 person2) {
+		int p2Ap = person2.getAp();
+		int setAp2 =(p2Ap += 3);
+		person2.setAp(setAp2);
+	}
+	
+	public void equipRevolver2P2(Person2 person2) {
+		int p2Ap = person2.getAp();
+		int setAp2 =(p2Ap += 7);
+		person2.setAp(setAp2);
+	}
+	
+	public void equipPistol1P2(Person2 person2) {
+		int p2Ap = person2.getAp();
+		int p2Speed = person2.getSpeed();
+		int setAp2 =(p2Ap += 2);
+		int setSpeed2 =(p2Speed += 5);
+		person2.setAp(setAp2);
+		person2.setSp(setSpeed2);
+		
+	}
+	
+	public void equipPistol2P2(Person2 person2) {
+		int p2Ap = person2.getAp();
+		int p2Speed = person2.getSpeed();
+		int setAp2 =(p2Ap += 4);
+		int setSpeed2 =(p2Speed += 10);
+		person2.setAp(setAp2);
+		person2.setSp(setSpeed2);
+	}
+	
+	public void equipHolsterP2(Person2 person2) {
+		int p2Speed = person2.getSpeed();
+		int setSpeed2 =(p2Speed += 15);
+		person2.setSp(setSpeed2);
+	}
+	
+	public void equipArmorP2(Person2 person2) {
+		int p2Sp = person2.getSp();
+		int setSp2 =(p2Sp += 2);
+		person2.setSp(setSp2);
 	}
 
 }
